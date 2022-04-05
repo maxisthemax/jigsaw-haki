69a62e1517ca77213e217c9b966efbbf474d1c8b
maxisthemax <maxisthemax89@gmail.com>
2022-04-05 09:50:56 +0800

    fix

--- pages/jigsaw.js
+++ pages/jigsaw.js
@@ -6,13 +6,14 @@ export default function Home() {
   const router = useRouter();
   const { url, rows, columns } = router.query;
   return (
-    <>
+    <div style={{ width: "100%", height: "100%" }}>
       <JigsawPuzzle
         imageSrc={url}
         rows={rows && rows > 0 ? rows : 6}
         columns={columns && columns > 0 ? columns : 6}
         onSolved={() => alert("Solved!")}
+        className="jigsaw-puzzle"
       />
-    </>
+    </div>
   );
 }
